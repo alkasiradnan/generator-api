658e02ce893f84da243d77ac76920859e6892800
alkasiradnan <alkasir.adnan@gmail.com>
2020-02-19 19:08:34 +0530

    placeholder added

--- resources/shared/DataTypes/URL.js
+++ resources/shared/DataTypes/URL.js
@@ -3,7 +3,7 @@
 module.exports = url = (name,entityName) => {
     return `<div className="form-group">
              <label> ${name} </label>
-            <input type="url" className="form-control" value = {${entityName.toLowerCase()}['${name}']} />
+            <input type="url" className="form-control" value = {${entityName.toLowerCase()}['${name}']} placeholder="http://google.com/"/>
         </div>`;
 };
 14
